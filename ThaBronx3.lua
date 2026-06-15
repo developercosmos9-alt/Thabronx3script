b8cf1ac698018ae7f6372e2c7a4b180954e053de
developercosmos9-alt <developercosmos9@gmail.com>
2026-06-15 19:18:53 +0100

    Refactor window creation in ThaBronx3.lua

--- ThaBronx3.lua
+++ ThaBronx3.lua
@@ -15,21 +15,3 @@ local Window = splib:MakeWindow({
  ToggleIcon = "rbxassetid://83114982417764",
  CloseCallback = true
 })
-
---[[
-Name = <string> - The name of the UI
-SubTitle = <string> - The sub name of the UI
-Setting = <bool> - Toggle to show the setting on the window
-Intro = <bool> - Enable/disable intro loading
-IntroText = <string> - Intro title text
-IntroIcon = <string> - Intro icon asset id
-IntroSpeed = <number> - Intro speed multiplier
-IsPremium = <bool> - If the player is has premium or not
-Toggle <bool> - This if you want enable/disable toggle
-Icon <string> - This if you want add a icon for your script
-RainbowMainFrame = <bool> - This if you want a RBG Stroke for MainFrame
-RainbowTitle = <bool> - This if you want a RBG Stroke for Title
-RainbowSubTitle = <bool> - This if you want a RBG Stroke for SubName
-ToggleIcon = <string> - URL to the image you want displayed on the toggle window
-CloseCallback = <function> - Function to execute when the window is closed
-]]
