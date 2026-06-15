15bee937d0ab6b85374a0be8bdec67688499e33c
developercosmos9-alt <developercosmos9@gmail.com>
2026-06-15 19:28:09 +0100

    Refactor Window initialization with splib loading
    Replaced the initialization of the Window variable with a new method call after loading the splib.

--- ThaBronx3.lua
+++ ThaBronx3.lua
@@ -1,3 +1,4 @@
+local splib = loadstring(game:HttpGet("https://raw.githubusercontent.com/as6cd0/SP_Hub/refs/heads/main/splibv2"))()
 local Window = splib:MakeWindow({
  Name = "SP Library v2",
  SubTitle = "by splib",
