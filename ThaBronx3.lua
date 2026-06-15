e3964caa13ecb42c85d0cf6425dcfe594e70ec95
developercosmos9-alt <developercosmos9@gmail.com>
2026-06-15 19:44:28 +0100

    Update ThaBronx3.lua

--- ThaBronx3.lua
+++ ThaBronx3.lua
@@ -1,11 +1,11 @@
 local splib = loadstring(game:HttpGet("https://raw.githubusercontent.com/as6cd0/SP_Hub/refs/heads/main/splibv2"))()
 
 local Window = splib:MakeWindow({
- Name = "SP Library v2",
+ Name = "XX Script v1",
  SubTitle = "by splib",
  Setting = true,
  Intro = true,
- IntroText = "SP Hub Loading",
+ IntroText = "XX script Loading",
  IntroIcon = "rbxassetid://83114982417764",
  IntroSpeed = 1,
  Toggle = true,
@@ -16,4 +16,39 @@ local Window = splib:MakeWindow({
  RainbowSubTitle = false,
  ToggleIcon = "rbxassetid://83114982417764",
  CloseCallback = true
+})
+
+local Tab = Window:MakeTab({
+  IsMobile = false,
+  IsPC = false,
+  Name = "Combat",
+  Icon = "rbxassetid://4483345998"
+})
+
+Tab:AddSection("Combat")
+
+Tab:AddButton({
+   IsMobile = false,
+   IsPC = false,
+   PremiumOnly = true,
+   HidePremium = false,
+   Name = "AimBot",
+   Desc = "What is this button do?",
+	Callback = function()
+      		
+  	end    
+})
+
+Tab:AddToggle({
+   IsMobile = false,
+   IsPC = false,
+   PremiumOnly = false,
+   HidePremium = false,
+   Name = "Toggle",
+   Desc = "What is this toggle do?",
+   Default = false,
+   Flag = "ToggleSave",
+	Callback = function(Value)
+		
+	end    
 })
